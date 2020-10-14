6e38e4ada925e2a13652c4dc4ad846cb9458c4be
Antonio Benc <antonio.benc@ecx.io>
2020-10-14 14:25:04 +0200

    Removing branches using Yes/No dialog

--- delete-branch-interactive.sh
+++ delete-branch-interactive.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-git branch
+function removeCurrentBranch {
+	sed -E '/\*/d'
+}
+
+function leftTrim {
+	sed -E 's/\*?[[:space:]]+//'
+}
+
+function askAndDelete {
+	read -p "Delete branch '$1'? (Y/n) " yn
+	case $yn in
+		[Nn]* ) ;;
+		* ) git branch -D $1;;
+	esac
+}
+
+all_branches=$(git branch | removeCurrentBranch | leftTrim)
+
+for i in $all_branches
+do
+	askAndDelete $i
+done 
